b8c3e743376ddc5bb692e18d317e46c51c30e85f
Dylan Markow <dylan@dylanmarkow.com>
2017-05-13 14:51:33 -0500

    Release first alpha

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoRanked.Mixfile do
 
   def project do
     [app: :ecto_ranked,
-     version: "0.1.0",
+     version: "0.1.0-alpha.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
