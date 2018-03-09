67700d11843b1961840688b17069d85159b23acd
Dylan Markow <dylan@dylanmarkow.com>
2018-03-09 08:40:54 -0600

    Publish 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoRanked.Mixfile do
 
   def project do
     [app: :ecto_ranked,
-     version: "0.1.7",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
